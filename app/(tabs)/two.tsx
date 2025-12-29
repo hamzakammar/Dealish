262318115bb684c35c8405b0c5841ac1a0782bef
Hamza <hamza.k.ammar@gmail.com>
2025-12-28 21:07:11 -0500

    Added Map Layout

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,14 +1,21 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet} from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import MapView from 'react-native-maps';
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <MapView style={styles.map} mapType="hybrid" 
+      initialRegion={{
+        latitude: 43.445750,
+        longitude: -80.578530,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }} />
     </View>
   );
 }
@@ -27,5 +34,9 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
